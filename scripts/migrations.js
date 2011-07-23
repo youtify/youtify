91b8179eb9218be29ba7d2e6bbde2b4429783b24
Per Thulin <per@youtify.com>
2011-07-24 01:20:07 +0200

    Avoid having to save playlists/settings together

--- scripts/migrations.js
+++ scripts/migrations.js
@@ -1,17 +1,12 @@
 function prefixWithLoggedOut() {
-    var playlists = localStorage['playlists'] || '[]';
-        settings = localStorage['settings'] || '{}';
+    if (localStorage['settings'] !== undefined) {
+        localStorage['loggedOutSettings'] = localStorage['settings'];
+        localStorage.removeItem('settings');
+    }
 
-    if (localStorage['loggedOut'] === undefined) {
-        localStorage['loggedOut'] = '{"playlists":' + playlists + ', "settings":' + settings + '}';
-
-        if (localStorage['settings'] !== undefined) {
-            localStorage.removeItem('settings');
-        }
-
-        if (localStorage['playlists'] !== undefined) {
-            localStorage.removeItem('playlists');
-        }
+    if (localStorage['playlists'] !== undefined) {
+        localStorage['loggedOutPlaylists'] = localStorage['playlists'];
+        localStorage.removeItem('playlists');
     }
 }
 
