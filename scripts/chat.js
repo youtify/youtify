d72a3a01c9eec76918886a06b33343c1eb78b3b6
Karl Tannergård <kalle@youtify.com>
2011-07-24 21:18:11 +0200

    Removed obsolete chat messages.

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,15 +1,6 @@
 var chatRev =  JSON.parse(localStorage['chatRev2'] || 0);
 
-var messages = [
-    'Godnatt :)',
-	'Nice!',
-	'Spanks :)',
-	'Toplists online!',
-	'Snyggt!! Jag kommer över om en stund OK?',
-	'Yes!',
-	'Tja, jag rensade lite och flyttade undan våra experiment filer och bilder. Slängde inget viktigt.',
-	'Visstja, Vår site är nästan lika stor som 3st 64kb demos. Det måste vi lösa.'
-]
+var messages = [];
 
 while (chatRev < messages.length) {
     alert(messages[chatRev]);
