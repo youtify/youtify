6128c9c2c23f6ce184d3764448a61af8273d9dbb
Per Thulin <per@youtify.com>
2011-09-04 15:50:53 +0200

    Parse links in video description

--- scripts/fatbar.js
+++ scripts/fatbar.js
@@ -8,6 +8,31 @@ function fatBar_Init() {
     }
 }
 
+/**
+ * First do regular XML escaping on input text, after parse links into <a> tags.
+ *
+ * From http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+ */
+function linkify(inputText) {
+    var replaceText, replacePattern1, replacePattern2, replacePattern3;
+
+    replacedText = inputText;
+
+    replacedText = replacedText.replace('&', '&amp;');
+    replacedText = replacedText.replace('<', '&lt;');
+    replacedText = replacedText.replace('>', '&gt;');
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(http?|https):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = replacedText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    return replacedText
+}
+
 var FatBar = {
     _lastLoadedVideoId: null,
 	loadFromVideo: function(video) {
@@ -89,7 +114,7 @@ var FatBar = {
 			var uri = data.entry.author[0].uri.$t;
             try {
                 var description = data.entry.media$group.media$description.$t;
-                $('#video-info-box .description').text(description);
+                $('#video-info-box .description').html(linkify(description));
             } catch (e) {
             }
 			$('#video-info-box .uploader')
