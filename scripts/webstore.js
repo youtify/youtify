b6f6cb202084da692fddd391f64ce3019b55202e
Karl Tannergård <github@kallux.com>
2011-11-08 19:57:07 +0100

    Removed unnecessary check

--- scripts/webstore.js
+++ scripts/webstore.js
@@ -4,12 +4,9 @@ function webstore_Init() {
 var ChromeWebStore = {
     appLink: 'https://chrome.google.com/webstore/detail/ceimdjnelbadcaempefhdpdhdokpnbho',
     suggestInstall: function() {
-        if (ChromeWebStore.isBrowserChrome() && !ChromeWebStore.isAppInstalled()) {
+        if (!ChromeWebStore.isAppInstalled()) {
             Notification.say('Youtify is available as an app in Chrome Web Store. Do you want to install it? <input type="button" onclick="javascript:event.stopPropagation();ChromeWebStore.installApp();" value="Install" />');
         }
-    },
-    isBrowserChrome: function() {
-        return navigator && navigator.userAgent && navigator.userAgent.indexOf('Chrome') != -1;
     },
     isAppInstalled: function() {
         return window.chrome && window.chrome.app && window.chrome.app.isInstalled;
@@ -18,10 +15,10 @@ var ChromeWebStore = {
         var fail = function() {
             console.log(arguments);
             Notification.say('Failed to install App. Please visit <a href="' + ChromeWebStore.appLink + '" target="_blank">Chrome Web Store</a> for installation.');
-        }
+        };
         var success = function() {
             Notification.say('Installation succeded!');
-        }
+        };
         if (window.chrome && window.chrome.webstore) {
             window.chrome.webstore.install(ChromeWebStore.appLink, success, fail);
         } else {
