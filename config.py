20ace9a4d88b967406e856af8b42f19805058e4d
Per Thulin <per@youtify.com>
2011-12-07 22:32:39 +0100

    Clean up config

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+import os
+
 CLIENT_ID = ''
 CLIENT_SECRET = ''
-FLATTR_URL = 'http://flattr.dev'
-#FLATTR_URL = 'https://flattr.com'
-REDIRECT_URL = 'http://localhost:8080/flattrback'
+
+ON_PRODUCTION = os.environ['SERVER_SOFTWARE'].startswith('Google App Engine') # http://stackoverflow.com/questions/1916579/in-python-how-can-i-test-if-im-in-google-app-engine-sdk
+
+if ON_PRODUCTION:
+    REDIRECT_URL = 'http://www.youtify.com/flattrback'
+else:
+    REDIRECT_URL = 'http://localhost:8080/flattrback'
