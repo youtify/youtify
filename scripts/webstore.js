f9cea049290d29ae2a29352a261bce1400fbb0f6
Per Thulin <per@perthulin.com>
2011-12-18 18:22:39 +0100

    Add webstore app install icon

--- scripts/webstore.js
+++ scripts/webstore.js
@@ -1,26 +1,35 @@
 function webstore_Init() {
-    ChromeWebStore.suggestInstall();
+    if (ChromeWebStore.isBrowserChrome() && !ChromeWebStore.isAppInstalled()) {
+        $('#top .menus .webstore').click(function() {
+            $(this).arrowPopup('#webstore-popup');
+        });
+
+        $('#webstore-popup a').one('click', function(event) {
+            event.preventDefault();
+            ChromeWebStore.installApp();
+        });
+    } else {
+        $('#top .menus .webstore').hide();
+    }
 }
 var ChromeWebStore = {
     appLink: 'https://chrome.google.com/webstore/detail/ceimdjnelbadcaempefhdpdhdokpnbho',
-    suggestInstall: function() {
-        if (ChromeWebStore.isBrowserChrome() && !ChromeWebStore.isAppInstalled()) {
-            Notification.say('Youtify is available as an app in Chrome Web Store. Do you want to install it? <input type="button" onclick="javascript:event.stopPropagation();ChromeWebStore.installApp();" value="Install" />');
-        }
-    },
+
     isBrowserChrome: function() {
         return navigator && navigator.userAgent && navigator.userAgent.indexOf('Chrome') != -1;
     },
+
     isAppInstalled: function() {
         return window.chrome && window.chrome.app && window.chrome.app.isInstalled;
     },
+
     installApp: function() {
         var fail = function() {
-            console.log(arguments);
-            Notification.say('Failed to install App. Please visit <a href="' + ChromeWebStore.appLink + '" target="_blank">Chrome Web Store</a> for installation.');
+            Notification.say('Failed to install App.');
         }
         var success = function() {
             Notification.say('Installation succeded!');
+            $('#top .menu .webstore').hide();
         }
         if (window.chrome && window.chrome.webstore) {
             window.chrome.webstore.install(ChromeWebStore.appLink, success, fail);
