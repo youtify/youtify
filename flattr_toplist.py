7dd4756830c21ab0a0b947a8dde7d7eed1cd0eec
Per Thulin <per@perthulin.com>
2012-01-29 13:39:58 +0100

    Fix flattr_toplist problem on production
    parse_qsl is imported from a different module
    in Python 2.5.

--- flattr_toplist.py
+++ flattr_toplist.py
@@ -1,9 +1,16 @@
+import logging
 import urlparse
 from google.appengine.api import urlfetch
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from django.utils import simplejson
+try:
+    from urlparse import parse_qsl
+except ImportError:
+    from cgi import parse_qsl
+
+MEMCACHE_KEY = 'flattr_toplist'
 
 def fetch_toplist():
     json = []
@@ -18,7 +25,7 @@ def fetch_toplist():
             for thing in result['things']:
                 url = urlparse.urlparse(thing['url'])
                 if url.netloc.startswith('www.youtube.com'):
-                    params = dict(urlparse.parse_qsl(url.query))
+                    params = dict(parse_qsl(url.query))
                     if 'v' in params:
                         json.append({
                             'title': thing['title'],
@@ -31,19 +38,23 @@ def fetch_toplist():
 
 def get_or_create_toplist_json():
     """ Returns an empty playlist if anything goes wrong """
-    cache = memcache.get('flattr_toplist')
+    cache = memcache.get(MEMCACHE_KEY)
     if cache is not None:
         return cache
     try:
         json = fetch_toplist()
     except:
+        logging.error('Error generating %s' % (MEMCACHE_KEY))
         return '[]'
-    memcache.add('flattr_toplist', json, 3600*24)
+    memcache.add(MEMCACHE_KEY, json, 3600*24)
     return json
 
 class ToplistHandler(webapp.RequestHandler):
 
     def get(self):
+        if 'flush' in self.request.arguments():
+            memcache.delete(MEMCACHE_KEY)
+            logging.debug('Flushed memcache for %s' % MEMCACHE_KEY)
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(get_or_create_toplist_json())
 
