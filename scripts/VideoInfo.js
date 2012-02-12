35ac63dbb586ec02da1d442b07b1cf61f0328898
Per Thulin <per@perthulin.com>
2012-02-12 12:03:35 +0100

    Fix bug where timeline was not showing any time

--- scripts/VideoInfo.js
+++ scripts/VideoInfo.js
@@ -73,6 +73,8 @@ var VideoInfo = {
             video: video
         };
         $.getJSON(url, params, function(data) {
+            info.video.duration = data.duration;
+            info.video.title = data.title;
             info.title = data.title;
             info.thumbnail = data.artwork_url;
             info.description = data.description;
