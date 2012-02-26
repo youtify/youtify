2b7416d3af4d2d7c7eb2589088127d3918bc1228
Per Thulin <per@perthulin.com>
2012-02-27 00:26:36 +0100

    Remove dead getWeek code

--- scripts/Main.js
+++ scripts/Main.js
@@ -33,32 +33,6 @@ $(document).ajaxError(function (e, r, ajaxOptions, thrownError) {
     }
 });
 
-Date.prototype.getWeek = function (dowOffset) {
-	/*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
-	dowOffset = typeof(dowOffset) === 'int' ? dowOffset : 0; //default dowOffset to zero
-	var newYear = new Date(this.getFullYear(),0,1);
-	var day = newYear.getDay() - dowOffset; //the day of week the year begins on
-	day = (day >= 0 ? day : day + 7);
-	var daynum = Math.floor((this.getTime() - newYear.getTime() - (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
-	var weeknum;
-	//if the year starts before the middle of a week
-	if(day < 4) {
-		weeknum = Math.floor((daynum+day-1)/7) + 1;
-		if(weeknum > 52) {
-            var nYear, nday;
-			nYear = new Date(this.getFullYear() + 1,0,1);
-			nday = nYear.getDay() - dowOffset;
-			nday = nday >= 0 ? nday : nday + 7;
-			/*if the next year starts before the middle of
-			the week, it is week #1 of that year*/
-			weeknum = nday < 4 ? 1 : 53;
-		}
-	} else {
-		weeknum = Math.floor((daynum+day-1)/7);
-	}
-	return weeknum;
-};
-
 $(document).ready(function() {
     var settings = new Settings();
 	
