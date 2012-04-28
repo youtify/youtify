9e9d8a199766060b4dba405705fee12e0a378642
Per Thulin <per@perthulin.com>
2012-04-28 23:29:27 +0200

    Remove "flattrable" badge
    It didn't fit it and was not positioned correctly
    when video title was overflowing.

--- scripts/BottomPanel.js
+++ scripts/BottomPanel.js
@@ -1,23 +1,8 @@
 var BottomPanel = {
     init: function() {
-        var $badge = $('#bottom .info .badge');
-
         // TITLE
         EventSystem.addEventListener('video_info_fetched', function(info) {
             BottomPanel.setTitleText(info.title);
-        });
-
-        EventSystem.addEventListener('flattr_thing_for_twitter_account_found', function(info) {
-            $badge.text('flattrable').show();
-        });
-        EventSystem.addEventListener('flattr_thing_for_track_found', function(info) {
-            $badge.text('flattrable').show();
-        });
-        EventSystem.addEventListener('flattr_thing_for_uploader_found', function(info) {
-            $badge.text('flattrable').show();
-        });
-        EventSystem.addEventListener('video_started_playing_successfully', function(info) {
-            $badge.hide();
         });
 
         $('#bottom .info .title').click(function() {
