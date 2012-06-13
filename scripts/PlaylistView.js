816383d8320aaa537a6fb294566673d2c0f06a2f
Per Thulin <per@youtify.com>
2012-06-13 21:35:08 +0200

    Fix crash on user profile view
    The problem was when viewing your own profile and had unsynced/local
    playlists.

--- scripts/PlaylistView.js
+++ scripts/PlaylistView.js
@@ -64,7 +64,7 @@ var PlaylistView = {
             $box.append($unsubscribeButton);
         }
 
-        if(user.id === my_user_id && my_user_id === playlist.owner.id && playlist.remoteId !== null) {
+        if (playlist.remoteId !== null && user.id === my_user_id && my_user_id === playlist.owner.id) {
             var $privacyContainer = $('<div class="privacy"/>'),
                 $privacy = $('<input type="checkbox"/>'),
                 $privacyLabel = $('<label class="translatable"/>').text(TranslationSystem.get('Public'));
