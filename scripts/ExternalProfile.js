3dd9cdc4a025babbda78ede5fbc6a6781a1c3d0b
Karl Tannergård <github@kallux.com>
2012-07-04 16:32:23 +0200

    Fix for external profiles

--- scripts/ExternalProfile.js
+++ scripts/ExternalProfile.js
@@ -30,25 +30,33 @@ var ExternalProfile = {
     loadSoundCloudUser: function(username) {
         self = this;
         self.resetView();
-        
         history.pushState(null, null, '/soundcloud/' + username);
 
         $.get("http://api.soundcloud.com/resolve.json", {client_id: SOUNDCLOUD_API_KEY, url: "https://soundcloud.com/" + username}, function(resolveData) {
+            if (typeof(resolveData) === "string") {
+                resolveData = JSON.parse(resolveData);
+            }
+            
             $.get("http://api.soundcloud.com/users/" + resolveData.id + ".json", {client_id: SOUNDCLOUD_API_KEY}, function(userData) {
+                if (typeof(userData) === "string") {
+                    userData = JSON.parse(userData);
+                }
                 self.$view.find('h1').text(userData.full_name);
-                self.$view.find('.source').attr('href', userData.permalink_url).text(TranslationSystem.get('View on SoundCloud'));
+                self.$view.find('.source').text(TranslationSystem.get('View on SoundCloud')).attr('href', userData.permalink_url);
                 self.$view.find('.img').append($('<img src="' + userData.avatar_url + '"/>'));
                 self.$view.find('.description').text(userData.description);
             });
-
+            
             $.get("http://api.soundcloud.com/users/" + resolveData.id + "/tracks.json", {client_id: SOUNDCLOUD_API_KEY}, function(tracksData) {
+                if (typeof(tracksData) === "string") {
+                    tracksData = JSON.parse(tracksData);
+                }
                 var results = Search.getVideosFromSoundCloudSearchData(tracksData);
                 var $tracklist = self.$view.find('.tracklist');
                 $.each(results, function(i, video) {
                     video.createListView().appendTo($tracklist);
                 });
             });
-
             self.showView();
         });
     }
