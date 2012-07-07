7be2058c21a79058b4d742806065b98fe5e8f700
Karl Tannergård <github@kallux.com>
2012-07-07 10:47:55 +0200

    Fixed link bug in newsfeed

--- scripts/NewsFeed.js
+++ scripts/NewsFeed.js
@@ -7,7 +7,7 @@ var NewsFeed = {
         var $user = '<span class="user small link"><img src="' + actor.smallImageUrl + '"/><span clas="name">' + Utils.escape(actor.displayName) + '</span></span>';
         var $playlist = '<span class="link playlist">' + Utils.escape(playlist.title) + '</span>';
         var $div = $('<div class="activity">' + TranslationSystem.get('$user subscribed to your playlist $playlist', {$playlist: $playlist, $user: $user}) + '</div>'); 
-        $div.find('.link.user').click(playlist.owner.goTo);
+        $div.find('.link.user').click(actor.goTo);
         $div.find('.link.playlist').click(playlist.goTo);
         return $div;
     },
