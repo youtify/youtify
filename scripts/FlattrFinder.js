fb94a0564cd2fe1ca310f582ccf528e994124299
Per Thulin <per@youtify.com>
2012-07-17 22:46:22 +0200

    Cleanup

--- scripts/FlattrFinder.js
+++ scripts/FlattrFinder.js
@@ -1,26 +1,6 @@
 var FlattrFinder = {
     init: function() {
         EventSystem.addEventListener('video_info_fetched', FlattrFinder.findFlattrThingForTrack);
-        EventSystem.addEventListener('uploader_info_fetched', FlattrFinder.findFlattrThingForUploader);
-        EventSystem.addEventListener('artist_twitter_account_found', FlattrFinder.findFlattrThingForTwitterAccount);
-    },
-
-    findFlattrThingForTwitterAccount: function(twitterUrl) {
-        var screenName = twitterUrl.split('/')[3];
-        var url = 'https://api.twitter.com/1/users/show.json?screen_name=' + screenName +'&include_entities=true&callback=?';
-
-        console.log('looking up flattr thing for ' + twitterUrl);
-
-        $.getJSON(url, function(twitterData) {
-            var url = 'https://api.flattr.com/rest/v2/things/lookup/?q=' + encodeURIComponent(twitterUrl) + '&jsonp=?';
-            $.getJSON(url, function(flattrData) {
-                EventSystem.callEventListeners('flattr_thing_for_twitter_account_found', {
-                    sourceUrl: twitterUrl,
-                    thingId: flattrData.id || null,
-                    flattrs: flattrData.flattrs || 0
-                });
-            });
-        });
     },
 
     findFlattrThingForTrack: function(info) {
@@ -30,6 +10,7 @@ var FlattrFinder = {
 
         if (info.video.flattrThingId) {
             EventSystem.callEventListeners('flattr_thing_for_track_found', {
+                videoTitle: info.video.title,
                 sourceUrl: info.url,
                 thingId: info.video.flattrThingId,
                 flattrs: info.video.flattrs || 0
@@ -38,6 +19,7 @@ var FlattrFinder = {
             $.getJSON(url, function(data) {
                 if (data.message !== 'not_found') {
                     EventSystem.callEventListeners('flattr_thing_for_track_found', {
+                        videoTitle: info.video.title,
                         sourceUrl: info.url,
                         thingId: data.id || null,
                         flattrs: data.flattrs || 0
@@ -45,21 +27,5 @@ var FlattrFinder = {
                 }
             });
         }
-    },
-
-    findFlattrThingForUploader: function(info) {
-        var url = 'https://api.flattr.com/rest/v2/things/lookup/?q=' + encodeURIComponent(info.url) + '&jsonp=?';
-
-        console.log('looking up flattr thing for ' + info.url);
-
-        $.getJSON(url, function(data) {
-            if (data.message !== 'not_found') {
-                EventSystem.callEventListeners('flattr_thing_for_uploader_found', {
-                    sourceUrl: info.url,
-                    thingId: data.id || null,
-                    flattrs: data.flattrs || 0
-                });
-            }
-        });
     }
 };
