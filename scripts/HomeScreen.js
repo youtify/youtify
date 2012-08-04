831f7c2400ea124482996a314d7fd4dfb73b171c
Karl Tannergård <github@kallux.com>
2012-08-04 17:09:45 +0200

    Updated the home screen with artists mosaic

--- scripts/HomeScreen.js
+++ scripts/HomeScreen.js
@@ -1,13 +1,8 @@
 var HomeScreen = {
     $rightView: null,
     menuItem: null,
+    nbrOfArtists: 0,
     
-    artists: [
-      { displayName: 'Monstercat', name: 'monstercatmedia', id: '8553751', img: '/images/profiles/monstercat.png', type: 'soundcloud', backgroundColor: '#FFF' },
-      { displayName: 'UKF Dubstep', name: 'ukfdubstep', id: 'ukfdubstep', img: '/images/profiles/ukf.png', type: 'youtube', backgroundColor: '#000' },
-      { displayName: 'Robyn', name: 'robyn', id: '421860', img: '/images/profiles/robyn.png', type: 'soundcloud', backgroundColor: '#cbcbcb' }
-    ],
-
     init: function() {
         this.$rightView = $('#right > .home');
         this.menuItem = new MenuItem({
@@ -23,101 +18,46 @@ var HomeScreen = {
     },
 
     show: function() {
-        self = this;
-        // Only load when needed
-        if (self.$rightView.html().trim().length === 0) {
-            self.$rightView.html('');
-            LoadingBar.show();
-            history.pushState(null, null, '/');
-            
-            $.get('/home', function(data) {
-                self.$rightView.html(data);
-            });
-            LoadingBar.hide();
-        }
+        history.pushState(null, null, '/');
+        HomeScreen.fill();
     },
     
     fill: function() {
         var self = this,
             i = 0,
             artist = null,
-            $spinner = self.$rightView.find('.spinner .inner');
+            $spinner = self.$rightView.find('.spinner .inner'),
+            /* workaround for jquery width bug: http://bugs.jquery.com/ticket/9945 */
+            width = ($('#right .home').width() - 320) * 1.15, 
+            itemWidth = 88,
+            rows = 3,
+            nbrOfArtists = 0;
+        width = width < 528 ? 528 : width;
+        nbrOfArtists = Math.ceil(width/itemWidth) * rows;
         
-        for (i; i < HomeScreen.artists.length; i += 1) {
-            artist = HomeScreen.artists[i];
-            switch (artist.type) {
-                case 'soundcloud':
-                    self.addSoundCloudProfile(artist, $spinner);
-                    break;
-                case 'youtube':
-                    self.addYouTubeProfile(artist, $spinner);
-                    break;
-            }
+        if (self.nbrOfArtists === nbrOfArtists) {
+            return;
+        } else {
+            self.nbrOfArtists = nbrOfArtists;
+            $spinner.html('');
         }
-        $spinner.width(HomeScreen.artists.length * 550 - 50);
-    },
-    
-    addSoundCloudProfile: function(artist, $spinner) {
-        $.getJSON("http://api.soundcloud.com/users/" + artist.id + "/tracks.json?callback=?", {client_id: SOUNDCLOUD_API_KEY}, function(tracksData) {
-            if (!tracksData) {
-                return;
-            }
-            var results = Search.getVideosFromSoundCloudSearchData(tracksData),
-            $tracklist = $('<table class="tracklist"/>');
-            
-            $.each(results, function(j, video) {
-                if (j > 4) {
-                    return false;
-                }
-                video.createListView()
-                    .addClass('droppable')
-                    .addClass('draggable')
-                    .appendTo($tracklist);
-            });
-            HomeScreen.addView($tracklist, artist, $spinner);
-        });
-    },
-    
-    addYouTubeProfile: function(artist, $spinner) {
-        $.getJSON('https://gdata.youtube.com/feeds/api/users/' + artist.id + '/uploads?callback=?', {alt: 'json-in-script', v: 2}, function(data) {
-            if (data.feed.entry === undefined) {
-                return;
-            }
-            var results = Search.getVideosFromYouTubeSearchData(data),
-                $tracklist = $('<table class="tracklist"/>');
-            
-            $.each(results, function(j, video) {
-                if (j > 4) {
-                    return false;
-                }
-                video.createListView()
-                    .addClass('droppable')
-                    .addClass('draggable')
-                    .appendTo($tracklist);
-            });
-            HomeScreen.addView($tracklist, artist, $spinner);
-        });
-    },
-    
-    addView: function($tracklist, artist, $spinner) {
-        var $item = $('<div class="item"/>'),
-            $left = $('<div class="left"/>'),
-            $title = $('<div class="title"/>').text(artist.displayName),
-            $img = $('<img/>'),
-            $box = $('<div class="playlist-box"/>'),
-            $tracklistContainer = $('<div class="tracklist-container minimized"/>');
         
-        $tracklistContainer.append($tracklist);
-        $box.append($tracklistContainer);
-        $img.attr({'src': artist.img});
-        $title.click(function() {
-            ExternalUserPage.load(artist.type, artist.name);
+        $.getJSON('/api/external_users/top/' + nbrOfArtists, function(data) {
+            $.each(data, function(i, externalUser) {
+                var $item = $('<div class="item"/>'),
+                    $title = $('<div class="title"/>'),
+                    image = new Image();
+                if (externalUser.avatar_url) {
+                    image.onload = function() {
+                        $item.css({'opacity': '1'});
+                    };
+                    image.src = externalUser.avatar_url;
+                    $item.css({'background-image': 'url('+ externalUser.avatar_url + ')'});
+                }
+                $title.text(externalUser.username);
+                $item.append($title);
+                $spinner.append($item);
+            });
         });
-        $left.append($title);
-        $left.append($img);
-        $left.css({'background': artist.backgroundColor});
-        $item.append($left);
-        $item.append($box);
-        $spinner.append($item);
-    }
+    }    
 };
