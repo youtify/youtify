79c1184d18b7e0f11a315b4e8e31c74c3bf8b7f3
Per Thulin <per@youtify.com>
2012-08-13 00:21:21 +0200

    Remove forgotten dev alert

--- scripts/SearchStats.js
+++ scripts/SearchStats.js
@@ -13,7 +13,6 @@ var SearchStats = {
                 clearTimeout(self.timeout);
             }
             self.timeout = setTimeout(function() {
-                alert(q);
                 $.post(SEARCH_STATS_URL + '/entries', {q: q}, function(data) {
                     console.log(data);
                 });
