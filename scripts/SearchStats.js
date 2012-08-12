bca057ed1dba17bca07921742ef195fbe7462985
Per Thulin <per@youtify.com>
2012-08-13 00:18:41 +0200

    Only track complete search queries

--- scripts/SearchStats.js
+++ scripts/SearchStats.js
@@ -1,12 +1,23 @@
 var SearchStats = {
+    timeout: null,
+
     init: function() {
+        self = this;
+
         if (!SEARCH_STATS_URL) {
             return;
         }
+
         EventSystem.addEventListener('new_search_executed', function(q) {
-            $.post(SEARCH_STATS_URL + '/entries', {q: q}, function(data) {
-                console.log(data);
-            });
+            if (self.timeout) {
+                clearTimeout(self.timeout);
+            }
+            self.timeout = setTimeout(function() {
+                alert(q);
+                $.post(SEARCH_STATS_URL + '/entries', {q: q}, function(data) {
+                    console.log(data);
+                });
+            }, 5000);
         });
     }
 };
