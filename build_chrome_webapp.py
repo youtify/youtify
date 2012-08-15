2788141a66333b79afb460892f59858fe0b5a3bf
Per Thulin <per@youtify.com>
2012-08-15 15:50:20 +0200

    Write output to a zip file

--- build_chrome_webapp.py
+++ build_chrome_webapp.py
@@ -1,8 +1,11 @@
+from zipfile import ZipFile
 try:
     from jinja2 import Template
 except:
     print "Could not import Jinja2, run 'easy_install Jinja2'"
     exit()
+
+zipfile = ZipFile("webapp.zip", "w")
 
 def render_main_template():
     f = open('./html/index.html')
@@ -10,10 +13,7 @@ def render_main_template():
     f.close()
 
     html = template.render(og_tag='', url='', ON_PRODUCTION=True, ON_DEV=False, USE_PRODUCTION_JAVASCRIPT=True)
-
-    f = open('./chrome_webapp_index.html', 'w')
-    f.write(html.encode('utf-8'))
-    f.close()
+    zipfile.writestr('index.html', html.encode('utf-8'))
 
     print "Template rendered"
 
