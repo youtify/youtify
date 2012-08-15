fc36a49f307401804863bf088dde97afba1aeb8e
Per Thulin <per@youtify.com>
2012-08-16 00:26:54 +0200

    Initial version of manifest
    Also remove zip file as it's more important to
    be able to load the packaged app from a directory
    at this point (dev mode in Chrome requires it).

--- build_chrome_webapp.py
+++ build_chrome_webapp.py
@@ -1,11 +1,18 @@
-from zipfile import ZipFile
+import os.path
+from shutil import copyfile
 try:
     from jinja2 import Template
 except:
     print "Could not import Jinja2, run 'easy_install Jinja2'"
     exit()
 
-zipfile = ZipFile("webapp.zip", "w")
+output_dir = os.path.join('./', 'chrome_webstore')
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+def add_manifest():
+    copyfile('chrome_webstore_manifest.json', os.path.join(output_dir, 'chrome_webstore_manifest.json'))
 
 def render_main_template():
     f = open('./html/index.html')
@@ -13,8 +20,11 @@ def render_main_template():
     f.close()
 
     html = template.render(og_tag='', url='', ON_PRODUCTION=True, ON_DEV=False, USE_PRODUCTION_JAVASCRIPT=True)
-    zipfile.writestr('index.html', html.encode('utf-8'))
+    f = open(os.path.join(output_dir, 'index.html'), 'w')
+    f.write(html.encode('utf-8'))
+    f.close()
 
     print "Template rendered"
 
+add_manifest()
 render_main_template()
