23c85c0dfb7a95a3c5fc9be3f57569cd9fdf172c
Karl Tannergård <github@kallux.com>
2012-10-09 18:09:42 +0200

    Removed mobile test alerts

--- scripts/Main.js
+++ scripts/Main.js
@@ -72,22 +72,7 @@ $(document).ready(function() {
         $('#top .go-left').click(Utils.scrollLeft);
         $('#top .go-right').click(Utils.scrollRight);
         if (Utils.is320()) {
-            alert(
-                'window ' + window.height + 
-                ' body ' + $('body').height() + 
-                ' left-wrapper ' + $('.left-wrapper').height() + 
-                ' left ' + $('#left').height()
-            );
-                
             $('body').scrollTop(1);
-            
-            alert(
-                'window ' + window.height + 
-                ' body ' + $('body').height() + 
-                ' left-wrapper ' + $('.left-wrapper').height() + 
-                ' left ' + $('#left').height()
-            );
-            
         }
 
         playlistManager = new PlaylistsManager();
