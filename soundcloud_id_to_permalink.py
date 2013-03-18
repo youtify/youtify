dc664c6f0742500f1f8ed6f925ed09e3422fc44e
Karl Tannergård <github@kallux.com>
2013-03-18 23:51:28 +0100

    Fix string bug, which must have risen when we migrated to webapp2. Closes #229

--- soundcloud_id_to_permalink.py
+++ soundcloud_id_to_permalink.py
@@ -9,7 +9,7 @@ class Handler(webapp2.RequestHandler):
         id = self.request.get('id')
         response = urlfetch.fetch('https://api.soundcloud.com/tracks/' + id + '.json?consumer_key=206f38d9623048d6de0ef3a89fea1c4d')
         json = simplejson.loads(response.content)
-        self.redirect(json['permalink_url'])
+        self.redirect(str(json['permalink_url']))
 
 app = webapp2.WSGIApplication([
         ('/soundcloud_id_to_permalink', Handler),
